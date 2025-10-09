bbef4432550cf56f1b2dd5c4d3eb6502c5426ddd
simon <simon@popfun.xyz>
2025-10-10 00:29:43 +0800

    Update settings page layout: add responsive main layout, vertical separator, and improve structure with new component

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
-
+    import {Separator} from "$lib/components/ui/separator";
 </script>
 
-<h1>Settings</h1>
+
+<main class="flex flex-row w-full h-full items-center gap-1 p-4 lg:gap-2 lg:p-6 border-r transition-[width,height] ease-linear">
+    <div class="flex flex-1/3">
+        <div class="flex flex-col">
+            aaaa
+        </div>
+    </div>
+    <Separator orientation="vertical"/>
+    <div class="flex flex-2/3">
+        <h1>Settings</h1>
+    </div>
+</main>
+
