b2dcf7f79501f4ca4e84bda8b5c8d2ca7a24521d
Simon <fzpying@gmail.com>
2025-11-19 22:14:00 +0800

    feat: add `AppSidebar` and update `AppLayout` for improved layout structure
    - Introduced new `AppSidebar` component for enhanced sidebar management.
    - Updated `AppLayout` to integrate `AppSidebar` with `SidebarInset` for better content structuring.
    - Enhanced flexibility and modularity of layout components.

--- src/lib/components/layout/app-layout.svelte
+++ src/lib/components/layout/app-layout.svelte
@@ -2,6 +2,8 @@
     import AppLockLayout from "./app-lock-layout.svelte";
     import {WebviewWindowLabels} from "$lib/constants/webview-window-labels";
     import Titlebar from "$lib/components/layout/titlebar.svelte";
+    import AppSidebar from "$lib/components/layout/app-sidebar.svelte";
+    import {SidebarInset} from "$lib/components/ui/sidebar";
 
     let {children} = $props();
 </script>
@@ -9,6 +11,15 @@
 <main>
     <Titlebar windowLabels={WebviewWindowLabels.Main}/>
     <AppLockLayout>
-        {@render children?.()}
+        <AppSidebar variant="inset"/>
+        <SidebarInset>
+            <div class="flex flex-1 flex-col">
+                <div class="@container/main flex flex-1 flex-col gap-2">
+                    <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                        {@render children?.()}
+                    </div>
+                </div>
+            </div>
+        </SidebarInset>
     </AppLockLayout>
 </main>
---
+++ src/lib/components/layout/app-sidebar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+
+    import type {ComponentProps} from "svelte";
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import {
+        SidebarContent,
+        SidebarFooter,
+        SidebarHeader,
+        SidebarMenu,
+        SidebarMenuButton,
+        SidebarMenuItem
+    } from "$lib/components/ui/sidebar/index.js";
+
+
+    let {...restProps}: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root collapsible="offcanvas" {...restProps}>
+    <SidebarHeader>
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <SidebarMenuButton class="data-[slot=sidebar-menu-button]:!p-1.5">
+                    {#snippet child({props})}
+                        <a href="##" {...props}>
+                            <span class="text-base font-semibold">Acme Inc.</span>
+                        </a>
+                    {/snippet}
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        </SidebarMenu>
+
+    </SidebarHeader>
+    <SidebarContent>
+
+    </SidebarContent>
+    <SidebarFooter>
+
+    </SidebarFooter>
+</Sidebar.Root>
