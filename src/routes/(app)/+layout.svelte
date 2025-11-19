7bfa58331e54871e3a6017295fec4093bb07b42d
Simon <fzpying@gmail.com>
2025-11-19 21:56:40 +0800

    refactor: remove unused logic and imports in `+layout.svelte` and `+page.svelte`
    - Removed unnecessary imports and logic including `onMount`, `getCurrentWindow`, and `isAlwaysOnTop`.
    - Simplified and streamlined component definitions.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,28 +1,17 @@
 <script lang="ts">
     import {onDestroy, onMount} from "svelte";
-    import {getCurrentWindow} from "@tauri-apps/api/window";
     import {appState} from "$lib/api/api";
     import {appIsLocked, appStore} from "$lib/stores/stores";
     import type {AppStateResponse} from "$lib/api/types";
     import AppLayout from "$lib/components/layout/app-layout.svelte";
 
-    const appWindow = getCurrentWindow();
-
     let {children} = $props();
-    let isAlwaysOnTop = $state(false);
     let timer: number = 0;
-
-
-    const toggleAlwaysOnTop = () => {
-        appWindow.setAlwaysOnTop(!isAlwaysOnTop);
-        isAlwaysOnTop = !isAlwaysOnTop;
-    }
 
     const appStateChange = (v: AppStateResponse) => {
         appStore.set(v)
         appIsLocked.update(v => $appStore?.isLocked ? true : v)
     }
-
 
     onMount(() => {
         appState().then(appStateChange)
@@ -34,15 +23,11 @@
     onDestroy(() => {
         if (timer > 0) clearInterval(timer);
     })
-
-    $inspect($appStore, $appIsLocked)
 </script>
-
 
 <AppLayout>
     {@render children()}
 </AppLayout>
-
 
 <style lang="css">
 </style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-    import {onMount} from "svelte";
-    import {listAccounts} from "$lib/api/api";
     import AccountList from "$lib/components/AccountList.svelte";
-
-    onMount(() => {
-        listAccounts()
-    })
 </script>
 
 
