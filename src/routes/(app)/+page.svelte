ae2be1d587b19a41e53546e2a3c6738a1c70e679
Simon <fzpying@gmail.com>
2025-11-27 22:33:52 +0800

    feat: replace static account detail placeholder with dynamic rendering in `+page.svelte`
    - Removed hardcoded `detail` placeholder object and replaced it with `currentAccountState` for dynamic data rendering.
    - Enhanced avatar and account-specific detail display using conditional checks for fields like `website`, `Password`, and `UserName`.
    - Updated logic to dynamically render account detail fields only when available.

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -16,18 +16,6 @@
     import type {Entry} from "$lib/api/types";
 
     const sidebarWidth = Number(SIDEBAR_WIDTH.replace("rem", "")) * 16;
-
-    const detail = {
-        title: "My Google Account",
-        username: "abc@google.com",
-        createdAt: "2025-08-26",
-        password: "••••••••••",
-        otp: "297 • 324",
-        otpTTL: 7,
-        website: "https://accounts.google.com",
-        tag: "My Google Account",
-        lastEdited: "2025-08-26 11:34:54"
-    };
     let {data}: PageProps = $props();
     let accountsState: Entry[] = $state<Entry[]>([]);
     let currentAccountState = $state<Entry | null>(null);
@@ -47,12 +35,16 @@
                 {#each accountsState as account,index(index)}
                     <Item>
                         {#snippet child({props})}
-                            <a {...props}>
+                            <a {...props} onclick={()=>{ currentAccountState = account; }}>
                                 <ItemMedia>
                                     <Avatar>
-                                        <AvatarImage
-                                                src={`https://ui-avatars.com/api/?name=${account.fields.UserName}&format=svg&bold=true&background=random&rounded=true`}
-                                                class="grayscale"/>
+                                        {#if account.fields.website}
+                                            <AvatarImage class="grayscale"
+                                                         src={`https://logo.5io.cc/${URL.parse(account.fields.website)?.host}`}/>
+                                        {:else}
+                                            <AvatarImage class="grayscale"
+                                                         src={`https://ui-avatars.com/api/?name=${account.fields.UserName}&format=svg&bold=true&background=random&rounded=true`}/>
+                                        {/if}
                                         <AvatarFallback>{account.fields.UserName}</AvatarFallback>
                                     </Avatar>
                                 </ItemMedia>
@@ -74,11 +66,16 @@
                 <div class="flex flex-col gap-6 py-6 mx-auto w-full">
                     <div class="[.border-b]:pb-6 grid auto-rows-min grid-cols-[auto_1fr_auto] items-center gap-4 px-6">
                         <Avatar class="size-12 rounded-lg">
-                            <AvatarImage src="https://logo.clearbit.com/google.com"/>
-                            <AvatarFallback>G</AvatarFallback>
+                            {#if currentAccountState.fields.website}
+                                <AvatarImage src={`https://logo.5io.cc/${URL.parse(currentAccountState.fields.website)?.host}`}/>
+                            {:else}
+                                <AvatarImage class="grayscale"
+                                             src={`https://ui-avatars.com/api/?name=${currentAccountState.fields.UserName}&format=svg&bold=true&background=random&rounded=true`}/>
+                            {/if}
+                            <AvatarFallback>{currentAccountState?.fields.UserName}</AvatarFallback>
                         </Avatar>
                         <div class="flex flex-col">
-                            <CardTitle>{detail.title}</CardTitle>
+                            <CardTitle>{currentAccountState?.fields.Title}</CardTitle>
                         </div>
                         <div class="flex items-center gap-2">
                             <Button variant="ghost" size="icon">
@@ -90,66 +87,78 @@
                         </div>
                     </div>
                     <div class="flex flex-col px-6 gap-4">
-                        <div class="rounded-xl border">
-                            <div class="flex items-center justify-between gap-4 p-4">
-                                <div class="flex flex-col">
-                                    <span class="text-sm text-muted-foreground">UserName</span>
-                                    <span class="text-base">{detail.username}</span>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="rounded-xl border p-4">
-                            <div class="flex items-center justify-between">
-                                <div class="flex flex-col">
-                                    <span class="text-sm text-muted-foreground">Password</span>
-                                    <span class="tracking-widest">{detail.password}</span>
-                                </div>
-                                <div class="flex items-center gap-3">
-                                    <div class="flex items-center gap-1 text-xs">
-                                        <span class="size-2 rounded-full bg-emerald-500"></span>
-                                        <span>极佳</span>
+                        {#if currentAccountState?.fields.UserName}
+                            <div class="rounded-xl border">
+                                <div class="flex items-center justify-between gap-4 p-4">
+                                    <div class="flex flex-col">
+                                        <span class="text-sm text-muted-foreground">UserName</span>
+                                        <span class="text-base">{currentAccountState?.fields.UserName}</span>
                                     </div>
-                                    <Button variant="ghost" size="icon">
-                                        <Copy class="size-4"/>
-                                    </Button>
-                                    <Button variant="ghost" size="icon">
-                                        <Eye class="size-4"/>
-                                    </Button>
                                 </div>
                             </div>
-                        </div>
+                        {/if}
 
-                        <div class="rounded-xl border p-4">
-                            <div class="flex items-center justify-between">
-                                <div class="flex flex-col">
-                                    <span class="text-sm text-muted-foreground">OTP</span>
-                                    <span class="text-base">{detail.otp}</span>
+                        {#if currentAccountState?.fields.Password}
+                            <div class="rounded-xl border p-4">
+                                <div class="flex items-center justify-between">
+                                    <div class="flex flex-col">
+                                        <span class="text-sm text-muted-foreground">Password</span>
+                                        <span class="tracking-widest">{currentAccountState?.fields.Password}</span>
+                                    </div>
+                                    <div class="flex items-center gap-3">
+                                        <div class="flex items-center gap-1 text-xs">
+                                            <span class="size-2 rounded-full bg-emerald-500"></span>
+                                            <span>极佳</span>
+                                        </div>
+                                        <Button variant="ghost" size="icon">
+                                            <Copy class="size-4"/>
+                                        </Button>
+                                        <Button variant="ghost" size="icon">
+                                            <Eye class="size-4"/>
+                                        </Button>
+                                    </div>
                                 </div>
-                                <Badge variant="outline" class="text-xs">{detail.otpTTL}</Badge>
                             </div>
-                        </div>
+                        {/if}
 
-                        <div class="rounded-xl border p-4">
-                            <div class="flex items-center justify-between">
-                                <div class="flex flex-col">
-                                    <span class="text-sm text-muted-foreground">Website</span>
-                                    <a href={detail.website}
-                                       class="text-base underline underline-offset-4">{detail.website}</a>
+                        {#if currentAccountState?.fields.otp}
+                            <div class="rounded-xl border p-4">
+                                <div class="flex items-center justify-between">
+                                    <div class="flex flex-col">
+                                        <span class="text-sm text-muted-foreground">OTP</span>
+                                        <span class="text-base">{currentAccountState?.fields.otp}</span>
+                                    </div>
+                                    <Badge variant="outline"
+                                           class="text-xs">{currentAccountState?.fields.otpTTL}</Badge>
                                 </div>
-                                <Globe class="size-4 opacity-70"/>
                             </div>
-                        </div>
+                        {/if}
 
-                        <div class="rounded-xl border p-4">
-                            <span class="text-sm text-muted-foreground">Tags</span>
-                            <div class="mt-2">
-                                <Badge variant="secondary">{detail.tag}</Badge>
+                        {#if currentAccountState?.fields.website}
+                            <div class="rounded-xl border p-4">
+                                <div class="flex items-center justify-between">
+                                    <div class="flex flex-col">
+                                        <span class="text-sm text-muted-foreground">Website</span>
+                                        <a href={currentAccountState?.fields.website}
+                                           class="text-base underline underline-offset-4">{currentAccountState?.fields.website}</a>
+                                    </div>
+                                    <Globe class="size-4 opacity-70"/>
+                                </div>
                             </div>
-                        </div>
+                        {/if}
 
+                        {#if currentAccountState?.fields.tag}
+                            <div class="rounded-xl border p-4">
+                                <span class="text-sm text-muted-foreground">Tags</span>
+                                <div class="mt-2">
+                                    <Badge variant="secondary">{currentAccountState?.fields.tag}</Badge>
+                                </div>
+                            </div>
+                        {/if}
                         <Separator class="my-2 border-transparent"/>
-                        <div class="text-muted-foreground text-sm">Latest Update {detail.lastEdited}</div>
+                        <div class="text-muted-foreground text-sm">Latest
+                            Update {currentAccountState?.fields.lastEdited}</div>
                     </div>
 
                 </div>
